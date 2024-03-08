a97f439262c5c8bb6e18b3123e9b245217c0cac8
SAAD <96654865+0Sa-ad0@users.noreply.github.com>
2024-03-08 10:56:49 +0600

    Add files via upload

---
+++ mid/LAB TASK = 2/LAB = 2.ts
@@ -0,0 +1,46 @@
+/*
+Discuss among your group members and pick a user category and implement the 
+Pipes based on the corresponding rule given below. All the user must have their 
+own DTO file.
+
+USER CATEGORY 1:
+
+• Name Should be only contain Alphabets
+• Email Address field is required, and the input must contain @ and .xyz 
+domain
+• Password field is required, and it must contain one numeric character 
+• Validated NID number format 
+• Validate the NID image given is no more then 2 MB
+• Phone Number must match with following regex ^018-\d{7}$
+
+USER CATEGORY 2:
+
+• Name Should be at least 4 characters 
+• Email Address field is required, and the input must contain aiub.edu domain
+• Password field must be at least 6 character long and it must contain one 
+Uppercase character 
+• Validate gender given male or female.
+• Validate the Profile picture given has JPG extension
+• Phone Number field must contain only numbers
+
+USER CATEGORY 3:
+
+• Name Should be maximum 50 characters long
+• Email Address field must be at least 5 characters long , and must contain @
+• Password field must be at least 6 character long and it must contain one 
+Lowercase character 
+• Validate the education field is not empty
+• Validate the CV uploaded is in PDF format
+• Phone Number field must start with 0
+
+USER CATEGORY 4:
+
+• Name field should not contain any numbers 
+• Email Address field must not be at least 30 characters long, and must 
+contain @ and .
+• Password field is required and it must contain one of the special 
+character(@ or # or $ or &) 
+• Validate a Date given is valid date type
+• Validate Social media links (at least 1 platform linked, valid URL format).
+• Phone Number field must not be longer than 11 digits.
+*/
